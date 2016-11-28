aeeec6411ca83ab262007c111f746ebacbda9ead
Thiago Morais <Thiago Morais>
2016-11-27 23:33:56 -0300

    capitulo03-aula05 [v2]

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Platform, ionicBootstrap,MenuController} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 import {MenuTestPage} from './pages/menu-test/menu-test';
+
 
 
 @Component({
@@ -12,7 +13,7 @@ export class MyApp {
   pages: Array<{component:any,title: string,icon: string}>;
   rootPage: any = HomePage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, private menuCtrl:MenuController) {
     this.pages = [
 
     {component: HomePage, title: 'Home', icon: 'home'},
@@ -26,9 +27,18 @@ export class MyApp {
     });
   }
 
-  openPage(page: any) : void{
+  openPage(page: any, menuSide: string) : void{
     this.rootPage=page.component;
+    this.menuCtrl.close(menuSide);
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp, [],{
+
+  menuType: 'push',
+    platforms: {
+      ios: {
+        menuType: 'overlay',
+      }
+    }
+});
